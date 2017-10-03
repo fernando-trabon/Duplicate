cecdd13000d311c6081d4627d79f9f0c97c9df70
Oleksandr Skrypets <blizerod@gmail.com>
2017-10-03 18:01:05 +0300

    HomeWork #1 v2

--- SkrypetsO/HomeWork1.java
+++ SkrypetsO/HomeWork1.java
@@ -12,13 +12,14 @@ public class HomeWork1 {
 		double c1, c2, c3; // call price
 		double t1, t2, t3; // call time
 		
+		// task #1.1
 		
 		System.out.println("Input radiusFlowerBed: ");
 		radiusFlowerBed = Double.parseDouble(reader.readLine());
 		System.out.println("Flower Bed's perimeter = " + String.format("%.2f",2*Math.PI*radiusFlowerBed) );
 		System.out.println("Flower Bed's area = " + String.format("%.2f",Math.PI*radiusFlowerBed*Math.pow(radiusFlowerBed, 2)));
 	
-		// task #1.1
+		// task #1.2
 		
 		System.out.println("What is your name? ");
 		name = reader.readLine();
@@ -26,7 +27,7 @@ public class HomeWork1 {
 		address = reader.readLine();
 		System.out.println("Your name is - " + name + ". And you live in " + address + ".");
 	
-		// task #1.2
+		// task #1.3
 		
 		System.out.print("Input c1, $/min:");
 		c1 = Double.parseDouble(reader.readLine());
@@ -41,7 +42,7 @@ public class HomeWork1 {
 		System.out.print("Input t3, min:");
 		t3 = Double.parseDouble(reader.readLine());
 				
-		// task #1.3
+		
 		
 		System.out.println("Price of first call = " + String.format("%.2f", c1 * t1) + " $");
 		System.out.println("Price of second call = " + String.format("%.2f", c2 * t2) + " $");
