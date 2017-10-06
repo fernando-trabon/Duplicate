8214d25a98c70d5f0e319f10ea10eca7a7d331c5
Oleksandr Skrypets <blizerod@gmail.com>
2017-10-06 09:19:46 +0300

    Project homework3 v1

--- homework/lesson3/Main.java
+++ homework/lesson3/Main.java
@@ -1,9 +1,26 @@
 package javaCore.homework.lesson3;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Person person1 = new Person();
+		Person person2 = new Person("Oleg", 1975);
+		Person person3 = new Person();
+		Person person4 = new Person("Ira", 1983);
+		Person person5 = new Person();
+		
+		person1.input();
+		person2.changeName();
+		person3.input();
+		person5.input();
+		
+		person1.output();
+		person2.output();
+		person3.output();
+		person4.output();
+		person5.output();
 
 	}
 
--- homework/lesson3/Person.java
+++ homework/lesson3/Person.java
@@ -1,5 +1,77 @@
 package javaCore.homework.lesson3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
 public class Person {
+	private String name;
+	private int birthYear;
+	
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	// Create constructors
+
+	public Person() {
+	}
+
+	public Person(String name, int birthYear) {
+		this.name = name;
+		this.birthYear = birthYear;
+	}
+
+	// Create getters and setters
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	// Method for calculating age of person
+
+	public int calculateAge() {
+		Calendar calendar = Calendar.getInstance();
+		int nowYear = calendar.get(Calendar.YEAR);
+		return nowYear - this.birthYear;
+	}
+	
+	// Method enter all data of person
+	
+	public void input() throws IOException {
+		System.out.println("Input name of person: ");
+		this.setName(reader.readLine());
+		System.out.println("Input birthYear of person: ");
+		this.setBirthYear(Integer.parseInt(reader.readLine()));
+		
+	}
+	
+	
+	// Method give all information about person
+	
+	public void output() {
+		System.out.println("Person's name:      [" + this.name  + "]"  + '\n' +"Person's birthYear: [" +
+				this.birthYear + "]" + '\n' + "Person's age:       [" + this.calculateAge() + "]");
+		System.out.println("");
+		
+	}
+
+	// Method change person's name
+
+	public void changeName() throws IOException {
+		System.out.println("Input person's name(rename): " );
+		this.name = reader.readLine();
+	}
 
 }
